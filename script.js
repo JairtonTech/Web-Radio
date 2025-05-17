7605569594cfcef2e9fab31ef28d6013059a530e
JairtonTech <filhojairton@gmail.com>
2025-05-17 11:46:29 -0300

    {mensagem_do_commit}

--- script.js
+++ script.js
@@ -9,7 +9,35 @@ const musicDatabase = {
         { titulo: "construção", url: "https://webradio-jf.s3.sa-east-1.amazonaws.com/pop/Milli+Vanilli.mp3" },
         { titulo: "construção", url: "https://webradio-jf.s3.sa-east-1.amazonaws.com/pop/Rockwell.mp3" },
         { titulo: "construção", url: "https://webradio-jf.s3.sa-east-1.amazonaws.com/pop/Shangri-La+Frontier+2+season.mp3" }
-    ]
+    ],
+    vozao: [
+        { titulo: "Vozão", url: "https://webradio-jf.s3.sa-east-1.amazonaws.com/cearamor/20+Anos+de+Moral.mp3" },
+        { titulo: "Vozão", url: "https://webradio-jf.s3.sa-east-1.amazonaws.com/cearamor/A+Minha+Vida+%C3%89+Te+Seguir.mp3" },
+        { titulo: "Vozão", url: "https://webradio-jf.s3.sa-east-1.amazonaws.com/cearamor/Cear%C3%A1+Vivo+essa+Paix%C3%A3o.mp3" },
+        { titulo: "Vozão", url: "https://webradio-jf.s3.sa-east-1.amazonaws.com/cearamor/Cearamor+-+A+TOC+%C3%89+O+Poder.mp3" },
+        { titulo: "Vozão", url: "https://webradio-jf.s3.sa-east-1.amazonaws.com/cearamor/Cearamor+-+Alem%C3%A3o+Passa+Mal.mp3" },
+        { titulo: "Vozão", url: "https://webradio-jf.s3.sa-east-1.amazonaws.com/cearamor/Cearamor+-+Aquela+torcidinha.mp3" },
+        { titulo: "Vozão", url: "https://webradio-jf.s3.sa-east-1.amazonaws.com/cearamor/Cearamor+-+Liga+pro+zool%C3%B3gico.mp3" },
+        { titulo: "Vozão", url: "https://webradio-jf.s3.sa-east-1.amazonaws.com/cearamor/Cearamor+-+Se+tocar+o+expulsa+expulsa.mp3" },
+        { titulo: "Vozão", url: "https://webradio-jf.s3.sa-east-1.amazonaws.com/cearamor/Da+Lhe+Alvinegro.mp3" },
+        { titulo: "Vozão", url: "https://webradio-jf.s3.sa-east-1.amazonaws.com/cearamor/O+Cear%C3%A1+Faz+Parte+da+Minha+Vida%2C+a+Minha+Alegria+%C3%89+Te+Ver+em+Campo.mp3" },
+        { titulo: "Vozão", url: "https://webradio-jf.s3.sa-east-1.amazonaws.com/cearamor/M%C3%BAsica+Cearamor.mp3" },
+        { titulo: "Vozão", url: "https://webradio-jf.s3.sa-east-1.amazonaws.com/cearamor/Muito+Mais+Que+Um+V%C3%ADcio+-+Cear%C3%A1+Sporting+Club.mp3" },
+        { titulo: "Vozão", url: "https://webradio-jf.s3.sa-east-1.amazonaws.com/cearamor/Maior+da+Capital+%C3%89+Nois.mp3" },
+        { titulo: "Vozão", url: "https://webradio-jf.s3.sa-east-1.amazonaws.com/cearamor/Hino+Oficial+do+Cear%C3%A1.mp3" },
+        { titulo: "Vozão", url: "https://webradio-jf.s3.sa-east-1.amazonaws.com/cearamor/Hino+do+Cear%C3%A1+Oficial.mp3" },
+        { titulo: "Vozão", url: "https://webradio-jf.s3.sa-east-1.amazonaws.com/cearamor/Funk+%C3%89+o+Nosso+Ritmo.mp3" },
+        { titulo: "Vozão", url: "https://webradio-jf.s3.sa-east-1.amazonaws.com/cearamor/Expulsa+Expulsa.mp3" },
+        { titulo: "Vozão", url: "https://webradio-jf.s3.sa-east-1.amazonaws.com/cearamor/Eu+sou+Cear%C3%A1.mp3" },
+        { titulo: "Vozão", url: "https://webradio-jf.s3.sa-east-1.amazonaws.com/cearamor/e+niguem+Cala+esse+Nosso+Amor!!!!!!!!!!!!!.mp3" },
+        { titulo: "Vozão", url: "https://webradio-jf.s3.sa-east-1.amazonaws.com/cearamor/Eu+sou+Cear%C3%A1.mp3" },
+        { titulo: "Vozão", url: "https://webradio-jf.s3.sa-east-1.amazonaws.com/cearamor/Expulsa+Expulsa.mp3" },
+        { titulo: "Vozão", url: "https://webradio-jf.s3.sa-east-1.amazonaws.com/cearamor/Funk+%C3%89+o+Nosso+Ritmo.mp3" },
+        { titulo: "Vozão", url: "https://webradio-jf.s3.sa-east-1.amazonaws.com/cearamor/Hino+do+Cear%C3%A1+Oficial.mp3" },
+        { titulo: "Vozão", url: "https://webradio-jf.s3.sa-east-1.amazonaws.com/cearamor/Hino+Oficial+do+Cear%C3%A1.mp3" }
+    ],
+    
+
 };
 
 const audioPlayer = document.getElementById("audioPlayer");
